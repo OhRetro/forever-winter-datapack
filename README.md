58131a93fe37a77179077d3309f4bff0e39b105a
OhRetro <70819072+OhRetro@users.noreply.github.com>
2024-11-04 12:05:54 -0300

    overhaul

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
-# Forever Winter
+# ❄️ Forever Winter
+Turn your world into a winter wonderland! **Forever Winter** transforms all Overworld biomes into snowy landscapes, perfect for those who wants a _chilling_ world, a somewhat challenging world, an atmospheric world to take screenshots with that shaders of yours
 
-Bring the winter to your world forever!
+## ✅ Features
+- **All Biomes Covered in Snow:** All biomes' temperature have dropped to negatives and their foliage have changed, all hot biomes such as: Deserts, Badlands, Savannas and their variations are also cold and covered in snow
+- **Only the biomes are changed**: It's the only thing it changes, whether you want the world to be snowing forever or snowing normally is up to you
+- **Supported Biomes:** Vanilla, more is planned
+- **Supported Versions:** 1.21 - 1.21.3, more is planned
 
-## Supported Biomes:
-- Vanilla
+## 📸 Screenshots
+[More in the Gallery page](https://modrinth.com/project/forever-winter/gallery)
+
+### 🎍 Snowy Bamboo Jungle
+![image](https://cdn.modrinth.com/data/DUCm4YCg/images/acf1cd042cc71f81d0552bbf7f01dc7b82cf57e2.png)
+
+### 🌸 Snowy Cherry Grove
+![image](https://cdn.modrinth.com/data/DUCm4YCg/images/a5cf90b6dbc408733ade254500ab8f4d64c06379.png)
+
+### 🦒 Snowy Savanna
+![image](https://cdn.modrinth.com/data/DUCm4YCg/images/ac46bac253497c6a2d8e6167f0f05aab807af11f.png)
+
+## 📑 Plans
+- Support more versions, newer versions and older versions if possible. 
+- Support custom biomes, such as in [Terralith](https://modrinth.com/datapack/terralith).
---
+++ tools/biome.py
@@ -0,0 +1,69 @@
+import os
+import json
+
+# Snowy Taiga Settings
+TARGET_VALUES = {
+    "downfall": 0.4,
+    "temperature": -0.5,
+    "effects": {
+        "fog_color": 12638463,
+        "sky_color": 8625919,
+        "water_color": 4020182,
+        "water_fog_color": 329011,
+    },
+    "has_precipitation": True
+}
+
+# Merge dicts without overwriting
+def merge_dicts(dict_a: dict, dict_b: dict):
+    for key, value in dict_b.items():
+        if isinstance(value, dict) and key in dict_a and isinstance(dict_a[key], dict):
+            merge_dicts(dict_a[key], value)
+        else:
+            dict_a[key] = value
+            print(f"updated dict A \"{key}\" = {value}")
+            
+def filter(directory: str):
+    print("filtering biomes...")
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            file_path = os.path.join(directory, filename)
+            
+            with open(file_path, "r") as file:
+                data = json.load(file)
+
+            # Tries to filter out the nether and end biomes
+            if "minecraft:freeze_top_layer" not in json.dumps(data):
+                os.remove(file_path)
+                print(f"deleted not overworld biomes: {filename}")
+                
+            elif filename.startswith(("snowy_", "frozen_", "ice_")):
+                os.remove(file_path)
+                print(f"deleted already cold biomes: {filename}")
+
+
+def apply_values(directory: str):
+    print("applying values...")
+    for filename in os.listdir(directory):
+        if filename.endswith(".json"):
+            print(f"applying values to \"{filename}\"")
+            file_path = os.path.join(directory, filename)
+            
+            with open(file_path, "r") as file:
+                data: dict = json.load(file)
+
+            merge_dicts(data, TARGET_VALUES)
+
+            effects: dict = data.get("effects", {})
+            effects.pop("grass_color", None)
+            effects.pop("foliage_color", None)
+
+            with open(file_path, "w") as file:
+                json.dump(data, file, indent=4)
+                
+            print("")
+
+if __name__ == "__main__":
+    target_dir = r"D:\Projects\VSCode\forever-winter-datapack\datapacks\fw1_21\data\minecraft\worldgen\biome"
+    filter(target_dir)
+    apply_values(target_dir)
